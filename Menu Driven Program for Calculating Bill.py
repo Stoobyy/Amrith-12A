164cebfe5a89df08f85570ee2ee731b736d72e7b
Stooby with two y's <97161453+Stoobyy@users.noreply.github.com>
2022-10-09 20:54:24 +0400

    Update Menu Driven Program for Calculating Bill.py

--- Menu Driven Program for Calculating Bill.py
+++ Menu Driven Program for Calculating Bill.py
@@ -2,11 +2,11 @@ print('Mode\t\tDiscount\n1.Credit Card\t10% of discount\n2.Debit Card\t5% of dis
 a=int(input('Enter the bill amount:'))
 b=int(input('Enter mode of payment: '))
 if b==1:
-    discount=a-(a*10)/100
+    discount= a - ((a*10)/100)
 elif b==2:
-    discount=a-(a*5)/100
+    discount= a - ((a*5)/100)
 elif b==3:
-    discount=a-(a*2)/100
+    discount= a - ((a*2)/100)
 else:
-    discount=a
-print(f'Discount amount is {discount}')
+    discount= a
+print(f'Discount amount is {a - discount}\nNet Payable Amount is {discount}')
